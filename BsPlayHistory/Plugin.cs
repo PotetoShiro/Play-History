5451f8ac9fc377ee6e435b952a5eb16a69fd2789
PotetoShiro <65686993+PotetoShiro@users.noreply.github.com>
2021-07-13 22:33:29 -0300

    Plugin.Cs faltando

---
+++ BsPlayHistory/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using IPA;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BsPlayHistory
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("BsPlayHistory initialized.");
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+
+            Log.Info("Creating Component");
+            new GameObject("BSPlugin1Controller").AddComponent<BsPlayHistoryController>();
+            Log.Info("Component Created");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+
+        }
+    }
+
+        
+}
